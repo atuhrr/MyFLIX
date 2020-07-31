830711bfbd0aae19d462bcf114d829ea51239db4
Arthur Arruda <62748391+atuhrr@users.noreply.github.com>
2020-07-31 13:45:30 +0200

    Slider, public, e a lib slick

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/home/App.jsx';
 
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CadastroVideo from'./pages/cadastro/video/index.jsx';
+import CadastroCategoria from './pages/cadastro/categoria/index.jsx';
+
+
+const Pagina404 = () => (<div>Error 404</div>)
+//ou
+//<Route component={() => (<div>Página 404</div>)} />
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+     <Route path="/" component={Home} exact />
+     <Route path="/cadastro/video" component={CadastroVideo} />
+     <Route path="/cadastro/categoria" component={CadastroCategoria} />
+     <Route component={Pagina404} />
+  </Switch>
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
 
